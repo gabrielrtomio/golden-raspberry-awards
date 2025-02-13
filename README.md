30e0a593cf1cbd2ad8c220f9a1302a08fa030d40
Gabriel R. Tomio <gabrieltomio@protonmail.com>
2025-02-13 09:48:39 -0300

    finish project

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Decisões
 
-Inicei o projeto pela modelagem de dados, e pela população dos dados ao iniciar a aplicação.
+Inicei o projeto pela modelagem de dados, e pela população dos dados ao iniciar a aplicação. Construi a API, e criei o teste unitário para testar a rota requisitada pela especificação. Finalizei a consulta principal.
+
+# Seed
+
+Para alterar os dados de população ao iniciar aplicação, é preciso substituir o arquivo **seed.csv** na raiz deste projeto.
+
+# Servidor
+
+Para iniciar o servidor.
+
+```console
+$ npm start
+```
+
+Acesse a consulta através do método **GET** em http://localhost:3000/api/awards/intervals
+
+# Testes
+
+Para executar os testes.
+
+```console
+$ npm test
+```
--- tests/routes/LoadAwardsIntervalsRoute.test.ts
+++ tests/routes/LoadAwardsIntervalsRoute.test.ts
@@ -14,6 +14,42 @@ before(async () => {
 });
 
 describe("Load Awards Intervals", () => {
+  it("Deve haver 3 produtores para o filme 'A Madea Family Funeral'", async () => {
+    const [movie] = await Db.query<Array<{ rowid: number }>>(
+      "SELECT rowid FROM movies WHERE title = ?",
+      ["A Madea Family Funeral"]
+    );
+    const producers = await Db.query<Array<{ rowid: number }>>(
+      "SELECT rowid FROM movies_producers WHERE movie_id = ?",
+      [movie.rowid]
+    );
+    deepEqual(producers.length, 3);
+  });
+
+  it("Deve haver 3 studios para o filme 'Sex and the City 2'", async () => {
+    const [movie] = await Db.query<Array<{ rowid: number }>>(
+      "SELECT rowid FROM movies WHERE title = ?",
+      ["Sex and the City 2"]
+    );
+    const studios = await Db.query<Array<{ rowid: number }>>(
+      "SELECT rowid FROM movies_studios WHERE movie_id = ?",
+      [movie.rowid]
+    );
+    deepEqual(studios.length, 3);
+  });
+
+  it("Deve haver 7 produtores para o filme 'Dirty Love'", async () => {
+    const [movie] = await Db.query<Array<{ rowid: number }>>(
+      "SELECT rowid FROM movies WHERE title = ?",
+      ["Dirty Love"]
+    );
+    const producers = await Db.query<Array<{ rowid: number }>>(
+      "SELECT rowid FROM movies_producers WHERE movie_id = ?",
+      [movie.rowid]
+    );
+    deepEqual(producers.length, 7);
+  });
+
   it("Deve retornar Joel Silver no mínimo e Matheuw Vaughn no máximo", async () => {
     const response = await request(api).get("/api/awards/intervals").query({});
     deepEqual(response.body, {
